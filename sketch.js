5c98aa4e87cdf192c268af3f652b382f198d010e
Ashlesha120206 <71136254+Ashlesha120206@users.noreply.github.com>
2021-04-08 13:52:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var gameState = 0;
+var playerCount;
+
+var canvas, backgroundImage;
+
+var database;
+
+var form, player, game;
+
+function setup(){
+
+ 
+  canvas = createCanvas(400,400);
+  database = firebase.database();
+
+  game = new Game();
+  game.getState();
+  game.start();
+
+  
+}
+
+function draw(){
+  background("white");
+  
+} 
